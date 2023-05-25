6a8c0cc2c8515cedadc99e2c7abd45b67170e644
Matei Busui <matei.busui@ezlo.com>
2023-05-25 23:10:44 +0300

    Added pre-build hook and default all rule to build.mk

--- interfaces/build.mk
+++ interfaces/build.mk
@@ -1,8 +1,12 @@
 BUILD_DIR?=dist
 BUILD?=$(BUILD_DIR)/$(BUILD_PREFIX)/$(OUT)
 CLEAN_TARGETS+=$(BUILD_DIR)
-.PHONY:build
-build: $(BUILD_DIR)/$(BUILD_PREFIX) $(BUILD)
+
+all: pre-build build
+
+.PHONY:pre-build build
+
+build: pre-build $(BUILD_DIR)/$(BUILD_PREFIX) $(BUILD)
 
 $(BUILD_DIR)/$(BUILD_PREFIX): $(BUILD_DIR)
 
