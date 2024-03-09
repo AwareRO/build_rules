5e7723e35b599b7da65d2cf680b9f55b88e91d14
Matei Busui <matei@busui.ro>
2024-03-09 14:38:20 +0200

    Force version 1.0-27

--- genver.sh
+++ genver.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 
-only_patch() {
-	sed 's/[^-]*-//'
-}
+source $(aware-devtools.sh libpath)/genver.sh
+source $(aware-devtools.sh libpath)/debian.sh
 
-no_patch() {
-	sed 's/-.*//'
-}
+echo "1.0-27"
+exit
 
-inc_patch() {
-	local ver="$(cat -)"
-	local patch="$(echo $ver | only_patch)"
-	local rest="$(echo $ver | no_patch)"
-	[ -z "$res" ] && rest=1.0
-	patch=$(($patch + 1))
-	echo "$rest-$patch"
-}
-
-latest_pkg() {
-	local pkg_name="$1"
-	sed -n  's/.*href="'"${pkg_name}_"'\([^"]*\).deb".*/\1/p' | sort -V | tail -n 1
-}
-
-ver_v2() {
-	curl -v -u 'build-rules-repo:HX{EMB),'"'"'!:d-_bN4q' https://deb.aware.ro/debian/ 2>/dev/null |\
-		latest_pkg build-rules | inc_patch
-}
-
-ver_v3() {
-	curl -u 'build-rules-repo:HX{EMB),'"'"'!:d-_bN4q' \
-		https://deb.aware.ro/debian/dists/dev/main/binary-all/Packages.gz |\
-		gunzip | awk '/^Package:/ {p=0} /^Package: build-rules$/ {printf $2; p=1} /^Version:/ {if(p){print " "$2}}' |\
-		tail -n 1 | awk '{print $2}' | inc_patch
-}
-
-ver_v3
+USERNAME="build-rules-repo" PASSWORD='HX{EMB),'"'"'!:d-_bN4q' pkg_list_http \
+	"https://deb.aware.ro/debian/dists/dev/main/binary-all/Packages.gz" build-rules |\
+	latest_version | pkg_version | inc_patch
