9899faee9b7bf367932754aa4f69acea9deb8323
Matei Busui <matei@busui.ro>
2024-03-09 14:39:09 +0200

    Refactor genver.sh

--- genver.sh
+++ genver.sh
@@ -3,9 +3,6 @@
 source $(aware-devtools.sh libpath)/genver.sh
 source $(aware-devtools.sh libpath)/debian.sh
 
-echo "1.0-27"
-exit
-
 USERNAME="build-rules-repo" PASSWORD='HX{EMB),'"'"'!:d-_bN4q' pkg_list_http \
 	"https://deb.aware.ro/debian/dists/dev/main/binary-all/Packages.gz" build-rules |\
 	latest_version | pkg_version | inc_patch
