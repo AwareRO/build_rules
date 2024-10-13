d51d2db21f1cf525e0d7fac1c4c8427879590a70
Matei Busui <matei@busui.ro>
2024-10-13 03:29:41 +0300

    Fixed genver.sh

--- genver.sh
+++ genver.sh
@@ -5,4 +5,4 @@ source $(aware-devtools.sh libpath)/debian.sh
 
 USERNAME="build-rules-repo" PASSWORD='HX{EMB),'"'"'!:d-_bN4q' pkg_list_http \
 	"https://deb.aware.ro/debian/dists/dev/main/binary-all/Packages.gz" build-rules |\
-	latest_version | pkg_version | inc_patch
+	latest_version build-rules | pkg_version | inc_patch
