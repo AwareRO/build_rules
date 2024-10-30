92a040496e1a0944ae641c595fd3755c5d606c4b
Matei Busui <matei@busui.ro>
2024-10-30 21:52:08 +0200

    Removed github builder errors due to missing dependencies

--- genver.sh
+++ genver.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-source $(aware-devtools.sh libpath)/genver.sh
-source $(aware-devtools.sh libpath)/debian.sh
+default_version() {
+	echo "1.0-0"
+	exit 0
+}
+
+source $(aware-devtools.sh libpath)/genver.sh 2>/dev/null || default_version
+source $(aware-devtools.sh libpath)/debian.sh 2>/dev/null || default_version
 
 USERNAME="build-rules-repo" PASSWORD='HX{EMB),'"'"'!:d-_bN4q' pkg_list_http \
 	"https://deb.aware.ro/debian/dists/dev/main/binary-all/Packages.gz" build-rules |\
-	latest_version build-rules | pkg_version | inc_patch
+	latest_version build-rules | pkg_version | inc_patch || default_version
